627210f4196a10543ef3e9bcb563c9ab284b7a37
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-06-29 00:48:25 +0100

    Created Complaint & DebtsReminder model, controller

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Complaints;
+use Illuminate\Http\Request;
+
+class ComplaintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Complaints $complaints)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Complaints  $complaints
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Complaints $complaints)
+    {
+        //
+    }
+}

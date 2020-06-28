2fb13f1618c1a34e2217fa2a6475e7fb150011db
jeremiahiro <jeremiahiro@gmail.com>
2020-06-29 00:05:44 +0100

    added migration

---
+++ database/migrations/2020_06_28_225952_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('ts_ref_id');
+            $table->string('ref_transaction_type');
+            $table->string('customer_ref_code');
+            $table->string('amount');
+            $table->string('interest');
+            $table->string('total_amount');
+            $table->string('expected_pay_date');
+            $table->string('user_ref_id');
+            $table->string('store_ref_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
---
+++ database/migrations/2020_06_28_230435_create_otp_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_ref_code');
+            $table->string('otp_code');
+            $table->timestamp('activated_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp');
+    }
+}

abca6588ce249ef9d0828f1f8b006ee6b3182f7d
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-06-29 05:46:48 +0100

    Updated web.php with missing routes

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,24 @@ Route::get('/login', 'Auth\LoginController@storeOwnerLogin')->name('login');
 Route::get('/register', 'Auth\RegisterController@storeOwnerRegister')->name('register');
 Route::post('/login', 'Auth\LoginController@login'); //this will be changed in future to => store/login, user/login
 Route::post('/register', 'Auth\RegisterController@register');
+Route::post('/logout', 'Auth\RegisterController@logout')->name('logout');
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/about', 'HomeController@about')->name('about');
 Route::get('/faq', 'HomeController@faq')->name('faq');
 Route::get('/contact', 'HomeController@contact')->name('contact');
 
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/dashboard', 'DashboardController@home')->name('dashboard');
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function () {
+    Route::get('/', 'DashboardController@home')->name('dashboard');
+    Route::get('/customers', 'CustomerController@index')->name('customers');
+    Route::get('/transactions', 'CustomerController@index')->name('transactions');
+    Route::get('/stores', 'StoreController@index')->name('stores');
+    Route::get('/analytics', 'AnalyticsController@index')->name('analytics');
+    Route::get('/debts', 'DebtsController@index')->name('debts.reminder');
+    Route::get('/broadcast', 'BroadcastController@index')->name('broadcast');
+    Route::get('/settings', 'SettingsContrller@index')->name('settings');
+    Route::get('/users', 'UserController@index')->name('users');
+    Route::get('/assistants/add', 'AssistantsController@index')->name('assistants.add');
+    Route::get('/complaint/new', 'ComplaintsController@index')->name('complaint.form');
+    Route::get('/complaint', 'ComplaintsController@index')->name('complaint.log');
 });

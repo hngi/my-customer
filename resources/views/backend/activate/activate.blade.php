125b9f06418225db054b052078b367c2f63f2bbf
Kofi Mokome <kofimokome10@gmail.com>
2020-06-29 02:38:39 +0100

    Add Verify Token functionality

--- resources/views/backend/activate/activate.blade.php
+++ resources/views/backend/activate/activate.blade.php
@@ -19,7 +19,7 @@
                                     <div class="row">
                                         <div class="col-12 p-5">
                                             <div class="mx-auto mb-5">
-                                                <a href="{{-- route('home') --}}">
+                                                <a href="{{url('/')}}">
                                                     <img src="{{ ('/frontend/assets/images/fulllogo.png') }}" alt=""
                                                          height="auto"/> </a>
                                             </div>
@@ -84,7 +84,7 @@
 
                             <div class="row mt-3">
                                 <div class="col-12 text-center">
-                                    <p class="text-muted">Back to <a href="{{-- route('logout') --}}"
+                                    <p class="text-muted">Back to <a href="{{url('/logout')}}"
                                                                      class="text-primary font-weight-bold ml-1">Login</a>
                                     </p>
                                 </div> <!-- end col -->

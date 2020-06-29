4cc45a08ef6ca72197be54d17a626a4aacfde77c
Kofi Mokome <kofimokome10@gmail.com>
2020-06-29 02:34:04 +0100

    Add Verify Token functionality

---
+++ app/Http/Controllers/VerifyController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OTP;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class VerifyController extends Controller
+{
+    /**
+     * Display a verify page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('backend.activate.activate');
+    }
+
+    /**
+     * Verifies an OTP CODE.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function verify(Request $request)
+    {
+        $validation = Validator::make(request()->all(), [
+            'token' => 'required|numeric|digits:6',
+        ])->validate();
+
+        // $user_id = Auth::id();
+        $user_id = 1;
+        $token = $request->input('token');
+        $otp = OTP::where('user_ref_code', '=', $user_id)->where('otp_code', '=', $token)->get();
+        if (sizeof($otp) > 0) {
+            $otp[0]->activated_at = now();
+            $otp[0]->save();
+            return redirect('/');
+        } else {
+            return back()->withError('OTP Token not found');
+        }
+
+    }
+
+
+}
--- resources/views/backend/activate/activate.blade.php
+++ resources/views/backend/activate/activate.blade.php
@@ -19,7 +19,7 @@
                                     <div class="row">
                                         <div class="col-12 p-5">
                                             <div class="mx-auto mb-5">
-                                                <a href="{{ route('home') }}">
+                                                <a href="{{-- route('home') --}}">
                                                     <img src="{{ ('/frontend/assets/images/fulllogo.png') }}" alt=""
                                                          height="auto"/> </a>
                                             </div>
@@ -28,22 +28,32 @@
                                             <p class="text-muted mt-1 mb-5">
                                                 Please enter the 6-digit code we sent on your number as SMS
                                             </p>
-                                            <div class="alert alert-danger alert-dismissible" id="error"
-                                                 style="display: none">
-                                                <span id="error-message"> </span>
 
-                                            </div>
-                                            <div class="alert alert-success alert-dismissible" id="success"
-                                                 style="display: none">
-                                                <span id="success-message">Your account has been activated.
-                                                    <a href="{{ route('dashboard') }}}">Click Here if you were not redirected</a>
-                                                </span>
+                                            @if (session('success'))
+                                                <div class="alert alert-success">
+                                                    {{ session('success') }}
+                                                </div>
+                                            @endif
+                                            @if (session('error'))
+                                                <div class="alert alert-danger">
+                                                    {{ session('error') }}
+                                                </div>
+                                            @endif
+                                            @if ($errors->any())
+                                                <div class="alert alert-danger">
+                                                    @foreach ($errors->all() as $error)
+                                                        {{ $error }}
+                                                    @endforeach
 
-                                            </div>
-                                            <form action="#" class="authentication-form">
+                                                </div>
+                                            @endif
+
+                                            <form action="{{url('/auth/verify')}}" method="post"
+                                                  class="authentication-form">
+                                                @csrf
                                                 <div class="form-group mt-4">
                                                     <label class="form-control-label">Enter 6 digit code</label>
-                                                    <a href=""
+                                                    <a href="{{url('/auth/verify-email')}}"
                                                        class="float-right text-muted text-unline-dashed ml-1"
                                                        id="resend_code">resubmit
                                                         code</a>
@@ -53,18 +63,15 @@
                                                             <i class="icon-dual" data-feather="lock"></i>
                                                         </span>
                                                         </div>
-                                                        <input type="text" class="form-control" id="code"
+                                                        <input type="text" class="form-control" name="token" id="code"
                                                                placeholder="X X X X X X">
                                                     </div>
                                                 </div>
 
                                                 <div class="form-group mb-0 text-center">
-                                                    <button class="btn btn-primary btn-block" type="submit" id="verify">
+                                                    <button class="btn btn-primary btn-block" type="submit"
+                                                            id="verify">
                                                         Verify
-                                                    </button>
-                                                    <button class="btn btn-primary btn-block disabled" type="submit"
-                                                            id="verifying" style="display:none" disabled="disabled">
-                                                        Verifying ...
                                                     </button>
                                                 </div>
                                             </form>
@@ -77,7 +84,7 @@
 
                             <div class="row mt-3">
                                 <div class="col-12 text-center">
-                                    <p class="text-muted">Back to <a href="{{ route('logout') }}"
+                                    <p class="text-muted">Back to <a href="{{-- route('logout') --}}"
                                                                      class="text-primary font-weight-bold ml-1">Login</a>
                                     </p>
                                 </div> <!-- end col -->
@@ -107,74 +114,6 @@
 
     <script src="/backend/assets/build/js/intlTelInput.js"></script>
     <script>
-        $(document).ready(function ($) {
-            const verifying = $("#verifying");
-            const error = $("#error");
-            const error_message = $("#error-message");
-            const success = $("#success");
-            const success_message = $("#success-message");
-            let timer = 0;
-            $('#verify').click(function (e) {
-                e.preventDefault();
-                hide_messages();
-                $(this).hide();
-                verifying.show();
-                const data = {
-                    api_token: '{{$apiToken}}',
-                    token: $("#code").val()
-                };
-                $.post('https://dev.customerpay.me/auth/verify', data, (data) => {
-                    //success
-                    $(this).show();
-                    verifying.hide();
-                    success_message.html('Your account has been activated.<br/>' + '<a href="{{url('/admin/dashboard')}}">Click Here if you were not redirected</a>');
-                    success.show();
-                    window.location = "{{url('/admin/dashboard')}}";
-                }).fail((e) => {
-                    e = JSON.parse(e.responseText);
-                    error_message.text(e.message.errors[0].description);
-                    error.show();
-                    $(this).show();
-                    verifying.hide();
-                });
-            });
-            $("#resend_code").click(function (e) {
-                e.preventDefault();
-                hide_messages();
-                if (timer <= 0) {
-                    const data = {
-                        api_token: '{{$apiToken}}',
-                        phone: '{{$phoneNumber}}',
-                    };
-                    $.post('https://dev.customerpay.me/auth/verify-phone', data, (data) => {
-                        //success
-                        success_message.html('Your account has been sent. You can request a new code in 60 seconds');
-                        success.show();
-                        start_timer();
-                    }).fail((e) => {
-                        e = JSON.parse(e.responseText);
-                        error_message.text(e.message.errors[0].description);
-                        error.show();
-                    });
-                } else {
-                    error_message.text("You can request a new code in " + timer + " seconds");
-                    error.show();
-                }
-            });
-            function start_timer() {
-                timer = 60;
-                let interval = setInterval(() => {
-                    if (timer > 0)
-                        timer -= 1;
-                    else
-                        clearInterval(interval);
-                }, 1000);
-            }
-            function hide_messages() {
-                error.hide();
-                success.hide();
-            }
-        });
         var input = document.querySelector("#phone");
         window.intlTelInput(input, {
             // any initialisation options go here

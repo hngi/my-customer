9c9d3937bd433f8757dcb803c1798513d82c2c06
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-06-29 06:01:01 +0100

    fix: email key too long error when migrating database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('user_ref_id');
